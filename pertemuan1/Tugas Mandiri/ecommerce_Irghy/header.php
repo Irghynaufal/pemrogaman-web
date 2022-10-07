f904b88a4237d4a8df68e928849ad7fadeed950a
Irghynaufal <irghyadriann@gmail.com>
2022-10-07 09:57:18 +0700

    pertemuan1

---
+++ pertemuan1/Tugas Mandiri/ecommerce_Irghy/header.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+    <head lang="en">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>E-Commerce Irghy</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    </head>
+    <body>
+        <header>
+          <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-white border fixed-top">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+              IRSHOP
+
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+            </ul>
+            <form class="container-fluid">
+              <div class="input-group">
+                <input type="text" class="form-control" placeholder="Cari Produk" aria-label="Username" aria-describedby="basic-addon1">
+                <span class="input-group-text" id="basic-addon1">
+                  <button class="btn btn-outline-success" type="submit">Search</button>
+                </span>
+               
+              </div>
+            </form>
+            <ul className="navbar-nav ml-auto">
+              <div class="navbar-nav">
+                <li class="nav-item ">
+                  <a class="nav-link" href="#"><i class="fas fa-shopping-cart"></i></a>
+                </li>
+                <li class="nav-item ">
+                  <a class="nav-link" href="#"><i class="fas fa-bell"></i></a>
+                </li>
+
+                  <li class="nav-item ">
+                    <a class="nav-link" href="#"><i class="fas fa-envelope"></i></a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Dropdown
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <li><a class="dropdown-item" href="#">Action</a></li>
+                      <li><a class="dropdown-item" href="#">Another action</a></li>
+                      <li><hr class="dropdown-divider"></li>
+                      <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                  </li>
+              </div>
+
+            </ul>
+          </div>
+         
+        </div>
+      </nav>
+        </header>
---
+++ pertemuan1/Tugas Mandiri/ecommerce_Irghy/konten.php
@@ -0,0 +1,24 @@
+<div class="container">
+            <div class="row" style="text-align-last: center;">
+                <div class="col ">
+                    <img src="img/car-disc-brake-brembo-brake-pad-png-favpng-E2EMaatEcLfdBB5hNTeGu0WLz-removebg-preview.png" height="200px" width="250px"><br>Otomotif
+                </div>
+                <div class="col ">
+                    <img src="img/varsity-removebg-preview.png" height="220px" width="170px"><br>Varsity Pria
+                </div>
+                <div class="col ">
+                    <img src="img/3085549572c69341c6962ba941a7fe4c-removebg-preview.png" height="220px" width="220px"><br>Hoodie Wanita
+                </div>
+                <div class="col ">
+                    <img src="img/0d0ac56f84ea1697ff4a7b4cc320fa2a-hi-removebg-preview.png" height="220px" width="190px"><br>Komputer & Aksesoris
+                </div>
+                <div class="col ">
+                    <img src="img/my-galaxy-z-fold3-f926-5g-sm-f926bzkdxme-474118243-removebg-preview.png" height="220px" width="250px"><br>Handphone & Aksesoris
+                </div>
+                <div class="col ">
+                    <img src="img/598-5982306_jordan-proto-max-hd-png-download-removebg-preview.png" height="150px" width="270px"><br>Sepatu
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
